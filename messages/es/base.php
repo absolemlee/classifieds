d826054f2c4e6a317fa7ce400d1b07cc10fe5b15
Absolem Lee <83130100+absolemlee@users.noreply.github.com>
2024-12-15 20:31:24 -0400

    Create base.php

---
+++ messages/es/base.php
@@ -0,0 +1,6 @@
+<?php
+return [
+    'A module for creating and browsing classifieds.' => 'Un módulo para crear y explorar clasificados.',
+    'Adds classifieds to your space.' => 'Añade clasificados a tu espacio.',
+    'Adds classifieds to your profile.' => 'Añade clasificados a tu perfil.',
+];
