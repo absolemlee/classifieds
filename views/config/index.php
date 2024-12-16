e7197ac1b0379bee7c70d25e5c672129e3841de5
Absolem Lee <83130100+absolemlee@users.noreply.github.com>
2024-12-15 20:35:27 -0400

    Create index.php

---
+++ views/config/index.php
@@ -0,0 +1,8 @@
+<?php
+use yii\helpers\Html;
+
+?>
+<div class="classifieds-config">
+    <h1>Classifieds Module Configuration</h1>
+    <p>This is where module configuration options will appear.</p>
+</div>
